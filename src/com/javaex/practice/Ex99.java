23ef28fc501efec8672de0d0bb04dea34504444d
805-T <805-T@DESKTOP-TCTFN79>
2024-06-21 11:37:13 +0900

    afdawf

--- src/com/javaex/practice/Ex99.java
+++ src/com/javaex/practice/Ex99.java
@@ -1,12 +1,10 @@
 package com.javaex.practice;
 
-public class Ex07 {
+public class Ex99 {
 
 	public static void main(String[] args) {
-		
-		System.out.println("aaaaa");
+		// TODO Auto-generated method stub
 
-		
 	}
 
 }
