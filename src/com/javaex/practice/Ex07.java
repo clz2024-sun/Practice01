3bfa2006ed896c3db24828ec9f3d95d69af1c439
clz2024.sun <clz2024.sun@gmail.com>
2024-06-25 18:35:15 +0900

    자바연습문제1-변수,자료형,연산자

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -3,9 +3,18 @@ package com.javaex.practice;
 public class Ex07 {
 
 	public static void main(String[] args) {
-		
-		System.out.println("aaaaa");
 
+		/*
+		다음 코드에서 변수로 사용할 수 없는 경우 이유를 작성하세요.
+		
+		int true;						//예약어
+		int #_of_workers;				//#사용
+		int countOfLettersInString;
+		int	1stLevel1;					//숫자 변수의 앞에사용
+		int	person#;					//#사용
+		int _person;
+		*/
+		
 		
 	}
 
--- src/com/javaex/practice/Hello.java
+++ src/com/javaex/practice/Hello.java
@@ -1,10 +1,12 @@
 package com.javaex.practice;
 
-public class Ex99 {
+public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Hello World");
+		
+		
 	}
 
 }
